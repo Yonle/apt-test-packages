047fa0c4772209e4d570151b6c8c1e6ed3e7b419
Yonle <yonle@duck.com>
2022-08-14 23:24:05 +0700

    Okay, Log time.

--- autotest.sh
+++ autotest.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-for $p in $(apt list | cut -d"/" -f1); do
-	if echo $p | grep -qo "Listing\.\.\.\|apt\|coreutils\|bash\|dpkg"; then continue; fi
-	apt install -y $p
+for p in $(apt list | cut -d"/" -f1); do
+	if echo $p | grep -qo "Listing\.\.\.\|apt\|coreutils\|bash\|dpkg\|yes"; then continue; fi
+	echo "-----> Installing $p"
+	yes | apt install $p
 	for bin in $(dpkg -L $p | grep "/usr/bin/"); do
+		echo "-----> Executing $(basename $bin)"
 		fname=${p}-$(date -u)-$(basename $bin).log
 		timeout \
-			--signal=KILL 5 \
+			--signal=KILL 3 \
 			$bin 2> "$fname"
 		if echo $? | grep -qo "134\|139"; then
-			echo "Error for binary $bin, and has been stored to $fname."
+			echo "-----> Error for binary $bin, and has been stored to $fname."
 		else
-			echo "$bin is OK."
+			echo "-----> $(basename $bin) is OK."
 			rm "$fname"
 		fi
 		killall -9 $bin || killall -9 $(basename $bin)
